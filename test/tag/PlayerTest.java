42e86864432766bb3b4b90b60db63451dfb7fbef
mudde <mudde@10.50.130.84>
2017-10-11 10:18:55 +0200

    Junit

---
+++ test/tag/PlayerTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tag;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testGetName() {
+    }
+
+    @Test
+    public void testGetHealth() {
+    }
+
+    @Test
+    public void testChangeHP() {
+        int hp = 0;
+        assertEquals(4,hp+4);
+        
+    }
+
+    @Test
+    public void testGetBank() {
+    }
+
+    @Test
+    public void testAddCoins() {
+    }
+    
+}
