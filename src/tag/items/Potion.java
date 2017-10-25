4cb14d18bb8ddabe9f2227d17abc057a15302769
rlumh <rlumh@10.50.136.215>
2017-10-25 12:08:53 +0200

    Potion fix

--- src/tag/items/Potion.java
+++ src/tag/items/Potion.java
@@ -1,17 +1,25 @@
 package tag.items;
 
 import tag.Player;
+import textio.SysTextIO;
+import textio.TextIO;
 
 public class Potion implements Item {
-    private String name = "Health potion";
-    private int healAmount = 10;
+    private final TextIO io = new TextIO(new SysTextIO());
+    private int healAmount;
+    private String name = "Health Potion (" + healAmount + " HP)";
     
     
-    public Potion() {}
+    public Potion(int healAmount) {
+        this.healAmount = healAmount;
+    }
 
     @Override
     public void effect(Player p) {
         p.changeHP(healAmount);
+        io.put("____________________________________________________________\n");
+        io.put("You healed yourself " + healAmount + " you now have " + p.getHealth() + " HP.\n");
+        io.put("____________________________________________________________\n");
         
     }
 
