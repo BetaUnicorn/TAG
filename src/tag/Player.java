59cbefa16157f28c187a12ac90a232591de8fb54
mudde <mudde@10.50.130.84>
2017-10-25 10:23:03 +0200

    hp

--- src/tag/Player.java
+++ src/tag/Player.java
@@ -1,11 +1,12 @@
 package tag;
 
 public class Player {
+
     private String name;
-    private int health = 10;
+    private int health = 100;
     private int bank = 0;
     private final Bag bag = new Bag();
-    
+
     public Player(String name) {
         this.name = name;
     }
@@ -13,8 +14,6 @@ public class Player {
     public Bag getBag() {
         return bag;
     }
-    
-    
 
     public String getName() {
         return name;
@@ -23,18 +22,26 @@ public class Player {
     public int getHealth() {
         return health;
     }
-    
-    public void changeHP(int hp) {
-        health += hp;
+
+    public void changeHP(Player p, int hp) {
+        if (p.getHealth() <= 100 && p.getHealth() > 0) {
+            health += hp;
+        }
+        else{
+            p.setHealth(100);
+        }
     }
 
     public int getBank() {
         return bank;
     }
-    
-    public void addCoins(int amount){
-         bank += amount;
-    
+
+    public void addCoins(int amount) {
+        bank += amount;
+
     }
-    
+    public void setHealth(int hp){
+        this.health = hp;
+    }
+
 }
