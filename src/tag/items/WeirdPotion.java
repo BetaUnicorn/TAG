70828a46f7f855f122bb356ff03baec858d864ec
rlumh <rlumh@DESKTOP-5INBJ65.home>
2017-10-26 15:59:01 +0200

    adds "Weird potion"

---
+++ src/tag/items/WeirdPotion.java
@@ -0,0 +1,30 @@
+package tag.items;
+
+import tag.Player;
+import textio.SysTextIO;
+import textio.TextIO;
+
+public class WeirdPotion implements Item {
+    private final TextIO io = new TextIO(new SysTextIO());
+    private final int dmgAmount;
+    private final String name;
+    
+    public WeirdPotion(int dmgAmount, String name) {
+        this.dmgAmount = dmgAmount;
+        this.name = (name + "(Looks like a potion, you've never seen before)");
+    }
+
+    @Override
+    public void effect(Player p) {
+        p.changeHP(-dmgAmount);
+        io.put("____________________________________________________________\n");
+        io.put("You took " + dmgAmount + " From the weird looking potion. \nYou now have " + p.getHealth() + " HP.\n");
+        io.put("____________________________________________________________\n");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+    
+}
