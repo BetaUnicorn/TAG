5933f84b2a674e633db45a977b373af1575c34e4
rlumh <rlumh@10.50.136.215>
2017-11-01 13:37:10 +0100

    Story and feeling update

--- src/tag/Setup.java
+++ src/tag/Setup.java
@@ -21,7 +21,7 @@ public class Setup {
     
     public ArrayList<NPC> newNpc(){
         ArrayList<NPC> monsters = new ArrayList<>();
-        monsters.add(new Monster("Lars", 10, new Weapon("Soul Edge", 10)));
+        monsters.add(new Monster("Lars, The Dragon", 10, new Weapon("Soul Edge", 10)));
         monsters.add(new Monster("Iron Fist", 30, new Weapon("Iron Fists", 20)));
         
         //Lars Inventory
@@ -41,63 +41,64 @@ public class Setup {
 
     public ArrayList<Room> createRooms() {
         ArrayList<Room> rooms = new ArrayList<>();
-
+ 
         rooms.add(new Room("Entrance", "In the room there is barely any light except for the few wall torches.\n"
                 + "when you look around, you see a room decayed by time.\n"
                 + "Skeletons on the ground covered in spiderwebs, and empty barrels. \n"
-                + "You feel disoriented but determined", ""));
-        rooms.add(new Room("Room1", p.getName() + " enters the room, it is bright enough so you can see clearly. \n"
-                + "you see two statues of dragons and a few empty crates and not much else.\n"
-                + "you feel discouraged but still curious.", "FEEEEELING"));
-        rooms.add(new Room("room2", p.getName() + " walks carefully into room.\n"
+                ,"feeling disoriented but determined"));
+        rooms.add(new Room("Pottery Room", p.getName() + " walks carefully into room.\n"
                 + "the room is filled with broken glass and pottery.\n"
-                + "Feeling Anxious", "FEEEEELING"));
-        rooms.add(new Room("Trap room", p.getName() + " walks proudly into the room. before you is a dragon's hoard of treasure. \n"
+                , "Feeling Anxious"));
+        rooms.add(new Room("Dragon Lair", p.getName() + " enters the room, it is bright enough so you can see clearly. \n"
+                + "you see two statues of dragons and a few empty crates and not much else.\n"
+                , "feeling discouraged but still curious."));
+        rooms.add(new Room("Unsuspicious room", p.getName() + " walks proudly into the room. before you is a dragon's hoard of treasure. \n"
                 + "Coins cover every inch of the room, \n"
                 + "and jeweled objects of precious metal jut up from the money like glittering islands in a sea of gold\n"
                 + "but wait!!! oh no!!!\n"
-                + "Feeling Stupid", "FEEEEELING"));
-        rooms.add(new Room("room4", p.getName() + " enters a room.\n"
+                , "feeling Stupid"));
+        rooms.add(new Room("Igloo Room", p.getName() + " enters a room.\n"
                 + "where there appears to be ice on the walls.\n"
-                + "Feeling Perplexed.", "FEEEEELING"));
-        rooms.add(new Room("room5", "This otherwise bare room has one distinguishing feature. \n"
+                , "feeling cold."));
+        rooms.add(new Room("Stone Room", "This otherwise bare room has one distinguishing feature. \n"
                 + "The stone around one of the other doors has been pulled over its edges, \n"
                 + "as though the rock were as soft as clay and could be moved with fingers.\n"
                 + "The stone of the door and wall seems hastily molded together.\n"
-                + "Feeling Tired", "FEEEEELING"));
-        rooms.add(new Room("room6", "This room holds six dry circular basins large enough to hold a man and a dry fountain at its center. \n"
+                , "feeling Tired"));
+        rooms.add(new Room("Bathroom", "This room holds six dry circular basins large enough to hold a man and a dry fountain at its center. \n"
                 + "All possess chipped carvings of merfolk and other sea creatures. \n"
                 + "It looks like this room once served some group of people as a bath.\n"
-                + "Feeling hopeful", "FEEEEELING"));
+                , "feeling hopeful"));
         rooms.add(new Room("Fire pit", "You open the door and a gout of flame rushes at your face. \n"
                 + "A wave of heat strikes you at the same time and light fills the hall. \n"
                 + "The room beyond the door is ablaze! \n"
                 + "An inferno engulfs the place, clinging to bare rock and burning without fuel.\n"
-                + "Feeling burned out", "FEEEEELING"));
-        rooms.add(new Room("room8", p.getName() + " walks into room\n"
+                , "feeling burned out"));
+        rooms.add(new Room("Spider Room", p.getName() + " walks into room\n"
                 + "Thick cobwebs fill the corners of the room,\n"
                 + "and wisps of webbing hang from the ceiling and waver in a wind you can barely feel\n"
-                + "Feeling Afraid", "FEEEEELING"));
-        rooms.add(new Room("room9", "you burst into the room\n"
+                , "feeling Afraid"));
+        rooms.add(new Room("Chamber", "you burst into the room\n"
                 + "This small bare chamber holds nothing but a large empty ironbound chest, \n"
                 + "which is big enough for a man to fit in.\n"
-                + "feeling dissapointed", "FEEEEELING"));
-        rooms.add(new Room("room10", p.getName() + " enters a room with a massive black throne, on it sits a\n"
+                , "feeling dissapointed"));
+        rooms.add(new Room("Throne Room", p.getName() + " enters a room with a massive black throne, on it sits a\n"
                 + "creature in full plate. The creature almost seems alive.\n"
-                + "Feeling Ready", "FEEEEELING"));
-        rooms.add(new Room("FinalRoom", "A tall granite door opens up. with countless runes all over.\n"
+                , "feeling Ready"));
+        rooms.add(new Room("Final Destination", "A tall granite door opens up. with countless runes all over.\n"
                 + "it is somehow untouched by the elements.\n"
                 + "you step closer to it to get a better look and it opens.\n"
                 + p.getName() + " enter into, the final Room of the dungeon.\n"
                 + "Before you stands a humongus Dragon, clad in crimson scales, \n"
                 + "like the colour of the fire it spews.\n"
                 + "you glance at it's long sharp claws, capable of slicing you apart with a single grace. \n"
-                + "and begin to realize you have no shred of a chance to survive. \n"
-                + "frigthened you try to push the door behind and escape but it won't budge. \n"
-                + "Behind the dragon you notice a door it's your only chance.\n"
-                + "you look at the dragon it hasen't noticed you. \n"
-                + "you run for the door in a desperate attempt, it worked you reached the door and you escaped with your life.\n"
-                + "Feeling Happy and Victorious", "FEEEEELING"));
+                + "The dragon gets a glimpse of you, and when it realizes that you are standing \n"
+                + "in its room. It starts charging against you. \n", "feeling nerveous"));
+                        
+//                + "Behind the dragon you notice a door it's your only chance.\n"
+//                + "you look at the dragon it hasen't noticed you. \n"
+//                + "you run for the door in a desperate attempt, it worked you reached the door and you escaped with your life.\n"
+//                + "Feeling Happy and Victorious", "FEEEEELING"));
 
         // sets connections between the rooms
         //Entrance connection to room 1
