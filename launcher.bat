4a67a765f86e8e086c9a9a7d4ebf51e7221dde58
mudde <mudde@10.50.130.84>
2017-11-02 08:45:28 +0100

    battle music

---
+++ launcher.bat
@@ -0,0 +1,2 @@
+java -jar "dist\TAG.jar"
+pause
--- src/tag/Combat.java
+++ src/tag/Combat.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import sun.audio.AudioData;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 import textio.SysTextIO;
@@ -16,7 +15,6 @@ public class Combat {
     private final TextIO io = new TextIO(new SysTextIO());
     AudioPlayer player = AudioPlayer.player;
     AudioStream stream;
-    AudioData data;
 
     public boolean combatScenario(Players p, NPC monster, Room currRoom, ArrayList<NPC> monsters) {
 
@@ -41,7 +39,7 @@ public class Combat {
 
     public void music() throws FileNotFoundException, IOException {
 
-        InputStream battleMusic = new FileInputStream("C:\\Users\\mudde\\Documents\\NetBeansProjects\\TAG\\long.wav");
+        InputStream battleMusic = new FileInputStream("long.wav");
 
         stream = new AudioStream(battleMusic);
         player.start(stream);
